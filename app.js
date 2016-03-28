ac39a6b4a13383bb564e4b739cb8e0b3bc20d6ea
Raul Camacho <rcamacho96@gmail.com>
2016-03-28 16:05:20 -0500

    added user auth to upload page

--- app.js
+++ app.js
@@ -125,7 +125,7 @@ app.post('/signup', userController.postSignup);
 app.get('/contact', contactController.getContact);
 app.post('/contact', contactController.postContact);
 app.get('/account', passportConf.isAuthenticated, userController.getAccount);
-app.get('/upload', userController.getUploadResume);
+app.get('/upload', passportConf.isAuthenticated, userController.getUploadResume);
 app.post('/account/profile', passportConf.isAuthenticated, userController.postUpdateProfile);
 app.post('/account/password', passportConf.isAuthenticated, userController.postUpdatePassword);
 app.post('/account/delete', passportConf.isAuthenticated, userController.postDeleteAccount);

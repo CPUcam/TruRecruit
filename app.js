f27607bdc96c6ef13290b6fc9b23e297460f9ed3
Raul Camacho <rcamacho96@gmail.com>
2016-04-07 15:06:09 -0500

    adding file upload using busboy as an alt to bodyparser

--- app.js
+++ app.js
@@ -74,7 +74,7 @@ app.use(logger('dev'));
 app.use(favicon(path.join(__dirname, 'public', 'favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(multer({dest:'./uploads/'});
+app.use(multer({dest:'./uploads/'}));
 app.use(expressValidator());
 app.use(methodOverride());
 app.use(cookieParser());

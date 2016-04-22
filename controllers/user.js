8d68767a8dc816b2575c9531eaa8f722b499f3f2
Raul Camacho <rcamacho96@gmail.com>
2016-04-22 14:54:33 -0500

    adding s3 integration

--- controllers/user.js
+++ controllers/user.js
@@ -128,7 +128,7 @@ exports.getAccount = function(req, res) {
  * Resume upload.
  */
 exports.getUploadResumePage = function(req, res) {
-  res.render('account/upload', {
+  res.render('/upload', {
     title: 'Resume'
   });
 };

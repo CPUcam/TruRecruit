230ad2f09d031cc8c07c33198e795cf5388aab3a
raulcodes <rcamacho96@gmail.com>
2016-11-06 13:46:42 -0600

    unstable mvp

---
+++ controllers/user.js
@@ -0,0 +1,558 @@
+const async = require('async');
+const crypto = require('crypto');
+const nodemailer = require('nodemailer');
+const passport = require('passport');
+const s3 = require('s3');
+const fs = require('fs');
+const User = require('../models/User');
+
+/*
+ * Client required for s3
+ *
+ */
+var client = s3.createClient({
+  maxAsyncS3: 20,     // this is the default
+  s3RetryCount: 3,    // this is the default
+  s3RetryDelay: 1000, // this is the default
+  multipartUploadThreshold: 20971520, // this is the default (20 MB)
+  multipartUploadSize: 15728640, // this is the default (15 MB)
+  s3Options: {
+    accessKeyId: process.env.S3KEYID,
+    secretAccessKey: process.env.S3ACCESSKEY,
+    // any other options are passed to new AWS.S3()
+    // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/Config.html#constructor-property
+  },
+});
+
+/*
+ * GET /upload
+ *
+ */
+exports.getUpload = (req, res) => {
+  res.render('account/upload', {
+    title: 'Upload'
+  });
+};
+
+function getRandom() {
+  var rand = [];
+
+  for (var i = 0; i < 6; i++) {
+    rand.push(Math.floor(Math.random() * 10));
+  }
+  return rand.join("");
+}
+
+/*
+ * POST /upload
+ * upload resume to s3 bucket
+ */
+exports.postUpload = (req, res, next) => {
+  console.log(req.file);
+
+  var rand = getRandom();
+  var boo = true;
+
+  User.findById(req.user.id, (err, user) => {
+    if (err) { return next(err); }
+  //   rand = '19121';
+  //
+  //   while (boo) {
+  //     User.findOne({ code: rand }, (err, rep) => {
+  //       if (rep) {
+  //         rand = getRandom();
+  //       }
+  //       else if (!rep) { boo = false; }
+  //     });
+  //   }
+  //
+    if (user.rCount >= 2) {
+      req.flash('errors', { msg: 'Basic members are only allowed two resume uploads.'});
+      return res.redirect('/upload');
+    } else {
+      user.rCount += 1;
+      console.log(user.rCount);
+      user.code = rand;
+      user.save((err) => {
+        if (err) { return next(err)}
+      });
+    }
+  });
+
+  req.flash('success', { msg: 'File was uploaded successfully.' });
+
+  // console.log(req.file);
+
+  var params = {
+    localFile: req.file.path,
+
+    s3Params: {
+      Bucket: "trurecruit",
+      Key: rand + ".pdf",
+      // other options supported by putObject, except Body and ContentLength.
+      // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#putObject-property
+    },
+  };
+
+  // console.log(rand);
+
+  var uploader = client.uploadFile(params);
+    uploader.on('error', function(err) {
+      console.error("unable to upload:", err.stack);
+    });
+    uploader.on('progress', function() {
+      console.log("progress", uploader.progressMd5Amount,
+                uploader.progressAmount, uploader.progressTotal);
+    });
+    uploader.on('end', function() {
+      console.log("done uploading");
+    });
+  fs.unlink(req.file.path, next);
+
+  //return res.redirect('/upload/code');
+};
+
+
+exports.getCode = (req, res) => {
+  res.render('account/code', {
+    title: 'Code'
+  });
+};
+
+/**
+ * GET /login
+ * Login page.
+ */
+exports.getLogin = (req, res) => {
+  if (req.user) {
+    return res.redirect('/');
+  }
+  res.render('account/login', {
+    title: 'Login'
+  });
+};
+
+/**
+ * GET /search
+ * Search for Resume.
+ */
+exports.getSearch = (req, res) => {
+  res.render('account/search', {
+    title: 'Search'
+  });
+};
+
+/**
+ * POST /search
+ * Search db and s3 bucket for user and resume
+ */
+exports.postSearch = (req, res, next) => {
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/search');
+  }
+
+  User.findOne( { code: req.body.code }, (err, user) => {
+    if (!user) {
+      req.flash('errors', { msg: 'User with code not found. '});
+      res.redirect('/search');
+    }
+    else {
+      var result = {
+        name: user.email,
+        code: user.code,
+        path: "/viewer/#../uploads/" + user.code + ".pdf"
+      };
+
+      fs.open("uploads/" + user.code + ".pdf", "r", (err, file) => {
+        if (file) {
+           return res.render('account/search', result);
+        }
+
+        else {
+          var params = {
+            localFile: "uploads/" + user.code + ".pdf",
+
+            s3Params: {
+              Bucket: "trurecruit",
+              Key: user.code + ".pdf",
+              // other options supported by getObject
+              // See: http://docs.aws.amazon.com/AWSJavaScriptSDK/latest/AWS/S3.html#getObject-property
+            },
+          };
+          var downloader = client.downloadFile(params);
+          downloader.on('error', function(err) {
+            console.error("unable to download:", err.stack);
+          });
+          downloader.on('progress', function() {
+            console.log("progress", downloader.progressAmount, downloader.progressTotal);
+          });
+          downloader.on('end', function() {
+            console.log("done downloading");
+          });
+          res.render('account/search', result);
+        }
+      });
+    }
+  });
+};
+
+
+
+/**
+ * POST /login
+ * Sign in using email and password.
+ */
+exports.postLogin = (req, res, next) => {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password cannot be blank').notEmpty();
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/login');
+  }
+
+  passport.authenticate('local', (err, user, info) => {
+    if (err) { return next(err); }
+    if (!user) {
+      req.flash('errors', info);
+      return res.redirect('/login');
+    }
+    req.logIn(user, (err) => {
+      if (err) { return next(err); }
+      req.flash('success', { msg: 'Success! You are logged in.' });
+      res.redirect(req.session.returnTo || '/');
+    });
+  })(req, res, next);
+};
+
+/**
+ * GET /logout
+ * Log out.
+ */
+exports.logout = (req, res) => {
+  req.logout();
+  res.redirect('/');
+};
+
+/**
+ * GET /signup
+ * Signup page.
+ */
+exports.getSignup = (req, res) => {
+  if (req.user) {
+    return res.redirect('/');
+  }
+  res.render('account/signup', {
+    title: 'Create Account'
+  });
+};
+
+/**
+ * POST /signup
+ * Create a new local account.
+ */
+exports.postSignup = (req, res, next) => {
+  req.assert('email', 'Email is not valid').isEmail();
+  req.assert('password', 'Password must be at least 4 characters long').len(4);
+  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/signup');
+  }
+
+  const user = new User({
+    email: req.body.email,
+    password: req.body.password
+  });
+
+  User.findOne({ email: req.body.email }, (err, existingUser) => {
+    if (existingUser) {
+      req.flash('errors', { msg: 'Account with that email address already exists.' });
+      return res.redirect('/signup');
+    }
+    user.save((err) => {
+      if (err) { return next(err); }
+      req.logIn(user, (err) => {
+        if (err) {
+          return next(err);
+        }
+        res.redirect('/');
+      });
+    });
+  });
+};
+
+/**
+ * GET /account
+ * Profile page.
+ */
+exports.getAccount = (req, res) => {
+  res.render('account/profile', {
+    title: 'Account Management'
+  });
+};
+
+/**
+ * POST /account/profile
+ * Update profile information.
+ */
+exports.postUpdateProfile = (req, res, next) => {
+  req.assert('email', 'Please enter a valid email address.').isEmail();
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/account');
+  }
+
+  User.findById(req.user.id, (err, user) => {
+    if (err) { return next(err); }
+    user.email = req.body.email || '';
+    user.profile.name = req.body.name || '';
+    user.profile.gender = req.body.gender || '';
+    user.profile.location = req.body.location || '';
+    user.profile.website = req.body.website || '';
+    user.save((err) => {
+      if (err) {
+        if (err.code === 11000) {
+          req.flash('errors', { msg: 'The email address you have entered is already associated with an account.' });
+          return res.redirect('/account');
+        }
+        return next(err);
+      }
+      req.flash('success', { msg: 'Profile information has been updated.' });
+      res.redirect('/account');
+    });
+  });
+};
+
+/**
+ * POST /account/password
+ * Update current password.
+ */
+exports.postUpdatePassword = (req, res, next) => {
+  req.assert('password', 'Password must be at least 4 characters long').len(4);
+  req.assert('confirmPassword', 'Passwords do not match').equals(req.body.password);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/account');
+  }
+
+  User.findById(req.user.id, (err, user) => {
+    if (err) { return next(err); }
+    user.password = req.body.password;
+    user.save((err) => {
+      if (err) { return next(err); }
+      req.flash('success', { msg: 'Password has been changed.' });
+      res.redirect('/account');
+    });
+  });
+};
+
+/**
+ * POST /account/delete
+ * Delete user account.
+ */
+exports.postDeleteAccount = (req, res, next) => {
+  User.remove({ _id: req.user.id }, (err) => {
+    if (err) { return next(err); }
+    req.logout();
+    req.flash('info', { msg: 'Your account has been deleted.' });
+    res.redirect('/');
+  });
+};
+
+/**
+ * GET /account/unlink/:provider
+ * Unlink OAuth provider.
+ */
+exports.getOauthUnlink = (req, res, next) => {
+  const provider = req.params.provider;
+  User.findById(req.user.id, (err, user) => {
+    if (err) { return next(err); }
+    user[provider] = undefined;
+    user.tokens = user.tokens.filter(token => token.kind !== provider);
+    user.save((err) => {
+      if (err) { return next(err); }
+      req.flash('info', { msg: `${provider} account has been unlinked.` });
+      res.redirect('/account');
+    });
+  });
+};
+
+/**
+ * GET /reset/:token
+ * Reset Password page.
+ */
+exports.getReset = (req, res, next) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  User
+    .findOne({ passwordResetToken: req.params.token })
+    .where('passwordResetExpires').gt(Date.now())
+    .exec((err, user) => {
+      if (err) { return next(err); }
+      if (!user) {
+        req.flash('errors', { msg: 'Password reset token is invalid or has expired.' });
+        return res.redirect('/forgot');
+      }
+      res.render('account/reset', {
+        title: 'Password Reset'
+      });
+    });
+};
+
+/**
+ * POST /reset/:token
+ * Process the reset password request.
+ */
+exports.postReset = (req, res, next) => {
+  req.assert('password', 'Password must be at least 4 characters long.').len(4);
+  req.assert('confirm', 'Passwords must match.').equals(req.body.password);
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('back');
+  }
+
+  async.waterfall([
+    function (done) {
+      User
+        .findOne({ passwordResetToken: req.params.token })
+        .where('passwordResetExpires').gt(Date.now())
+        .exec((err, user) => {
+          if (err) { return next(err); }
+          if (!user) {
+            req.flash('errors', { msg: 'Password reset token is invalid or has expired.' });
+            return res.redirect('back');
+          }
+          user.password = req.body.password;
+          user.passwordResetToken = undefined;
+          user.passwordResetExpires = undefined;
+          user.save((err) => {
+            if (err) { return next(err); }
+            req.logIn(user, (err) => {
+              done(err, user);
+            });
+          });
+        });
+    },
+    function (user, done) {
+      const transporter = nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USER,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
+      const mailOptions = {
+        to: user.email,
+        from: 'passwordReset@trurecruit.com',
+        subject: 'Your TruRecruit password has been changed',
+        text: `Hello,\n\nThis is a confirmation that the password for your account ${user.email} has just been changed.\n`
+      };
+      transporter.sendMail(mailOptions, (err) => {
+        req.flash('success', { msg: 'Success! Your password has been changed.' });
+        done(err);
+      });
+    }
+  ], (err) => {
+    if (err) { return next(err); }
+    res.redirect('/');
+  });
+};
+
+/**
+ * GET /forgot
+ * Forgot Password page.
+ */
+exports.getForgot = (req, res) => {
+  if (req.isAuthenticated()) {
+    return res.redirect('/');
+  }
+  res.render('account/forgot', {
+    title: 'Forgot Password'
+  });
+};
+
+/**
+ * POST /forgot
+ * Create a random token, then the send user an email with a reset link.
+ */
+exports.postForgot = (req, res, next) => {
+  req.assert('email', 'Please enter a valid email address.').isEmail();
+  req.sanitize('email').normalizeEmail({ remove_dots: false });
+
+  const errors = req.validationErrors();
+
+  if (errors) {
+    req.flash('errors', errors);
+    return res.redirect('/forgot');
+  }
+
+  async.waterfall([
+    function (done) {
+      crypto.randomBytes(16, (err, buf) => {
+        const token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function (token, done) {
+      User.findOne({ email: req.body.email }, (err, user) => {
+        if (!user) {
+          req.flash('errors', { msg: 'Account with that email address does not exist.' });
+          return res.redirect('/forgot');
+        }
+        user.passwordResetToken = token;
+        user.passwordResetExpires = Date.now() + 3600000; // 1 hour
+        user.save((err) => {
+          done(err, token, user);
+        });
+      });
+    },
+    function (token, user, done) {
+      const transporter = nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: process.env.SENDGRID_USER,
+          pass: process.env.SENDGRID_PASSWORD
+        }
+      });
+      const mailOptions = {
+        to: user.email,
+        from: 'passwordReset@trurecruit.com',
+        subject: 'Reset your password on TruRecruit',
+        text: `You are receiving this email because you (or someone else) have requested the reset of the password for your account.\n\n
+          Please click on the following link, or paste this into your browser to complete the process:\n\n
+          http://${req.headers.host}/reset/${token}\n\n
+          If you did not request this, please ignore this email and your password will remain unchanged.\n`
+      };
+      transporter.sendMail(mailOptions, (err) => {
+        req.flash('info', { msg: `An e-mail has been sent to ${user.email} with further instructions.` });
+        done(err);
+      });
+    }
+  ], (err) => {
+    if (err) { return next(err); }
+    res.redirect('/forgot');
+  });
+};
